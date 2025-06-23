bf9f1a4ca9b5e0a09a89c597af8a4f87a2fd06d8
sharabh mishra <sharabhmishra15@gmail.com>
2025-06-23 23:40:53 +0530

    adding linked list, new node added, print list added, append added, pop added and prepend added

---
+++ Linked List/1_print_LL.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.head is None:
+            return None
+
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.length +=1
+            return new_node
+        
+        new_node.next = self.head
+        self.head = new_node
+        self.length +=1
+        return new_node
+
+py = LinkedList(2)
+py.append(3)
+py.append(4)
+py.append(1)
+py.append(7)
+py.append(7)
+py.prepend(9)
+py.print_list()
+
+print(f"length is: {py.length}")

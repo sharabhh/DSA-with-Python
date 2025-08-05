f9fe3db81da5dd4fd8373e20829c9ce42af53900
sharabh mishra <sharabhmishra15@gmail.com>
2025-08-05 19:18:10 +0530

    append a node to doubly linekd list

--- Doubly Linked List/print_list.py
+++ Doubly Linked List/print_list.py
@@ -17,6 +17,21 @@ class DoublyLinekdList:
             print(temp.value)
             temp = temp.next
 
+    def append_item(self, value):
+        new_node = Node(value)
+        if self.tail is not None:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            self.length +=1
+        else:
+            self.head = new_node
+            self.tail = new_node
+        self.length +=1
+        print(f"The new length is {self.length}")
+        return True
+
 obj1 = DoublyLinekdList(7)
+obj1.append_item(2)
 
 obj1.print_list()

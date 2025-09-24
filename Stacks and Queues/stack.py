8152c2c9c6fd946e56d0874f1ab3331e5742f91f
sharabh mishra <sharabhmishra15@gmail.com>
2025-09-24 21:55:57 +0530

    refactor push_stack method in Stack class for clarity and consistency

--- Stacks and Queues/stack.py
+++ Stacks and Queues/stack.py
@@ -9,6 +9,19 @@ class Stack:
         self.top = new_node
         self.height = 1
     
+    def push_stack(self, value):
+            
+        new_node = Node(value)
+        if(self.height == 0):
+            self.top = new_node
+            return
+        else:
+            new_node.next = self.top
+            self.top = new_node
+            self.height += 1
+        self.height = 1
+        return
+
     def print_stack(self):
         temp = self.top
         while temp is not None:
@@ -16,4 +29,7 @@ class Stack:
             temp = temp.next
 
 my_stack = Stack(5)
+my_stack.push_stack(2)
+my_stack.push_stack(1)
+my_stack.push_stack(26)
 my_stack.print_stack()

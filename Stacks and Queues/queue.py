48a378ae2920e8f511b626f2d539928d64536fa4
sharabh mishra <sharabhmishra15@gmail.com>
2025-09-24 22:35:59 +0530

    add initial implementation of Queue and Node classes along with enqueue

---
+++ Stacks and Queues/queue.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.height = 1
+
+    def enqueue(self, value):
+        new_node = Node(value)
+
+        if(self.height == 0):
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.height +=1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        return
+
+myqueue = Queue(4)
+myqueue.enqueue(2)
+myqueue.enqueue(7)
+myqueue.enqueue(1)
+myqueue.print_queue()

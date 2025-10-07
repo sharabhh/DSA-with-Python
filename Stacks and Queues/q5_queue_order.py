d396d1fc7a04a23ce73be385f4e1da3bf2e34090
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-07 22:07:00 +0530

    implement dequeue method and update comments for queue implementation

--- Stacks and Queues/q5_queue_order.py
+++ Stacks and Queues/q5_queue_order.py
@@ -1,9 +1,12 @@
-# You are given a class MyQueue which implements a queue using two stacks. Your task is to implement the enqueue method which should add an element to the back of the queue.
+# a) You are given a class MyQueue which implements a queue using two stacks. Your task is to implement the enqueue method which should add an element to the back of the queue.
 # To achieve this, you can use the two stacks stack1 and stack2. Initially, all elements are stored in stack1 and stack2 is empty. In order to add an element to the back of the queue, you need to first transfer all elements from stack1 to stack2 using a loop that pops each element from stack1 and pushes it onto stack2.
 # Once all elements have been transferred to stack2, push the new element onto stack1. Finally, transfer all elements from stack2 back to stack1 in the same way as before, so that the queue maintains its ordering.
 # Your implementation should satisfy the following constraints:
 # The method signature should be def enqueue(self, value).
 # The method should add the element value to the back of the queue.
+
+# b) You have been tasked with implementing a queue data structure using two stacks in Python, and you need to write the dequeue method.
+# The dequeue method should remove and return the first element in the queue
 
 class Stack:
     def __init__(self):
@@ -36,11 +39,21 @@ class Stack:
         while not new_stack.is_empty():
             self.append_stack(new_stack.pop_stack())
         return
+    
+    def dequeue(self):
+        new_stack = Stack()
+        while not self.is_empty():
+            new_stack.append_stack(self.pop_stack())
+        popped_element = new_stack.pop_stack()
+        while not new_stack.is_empty():
+            self.append_stack(new_stack.pop_stack())
+        return popped_element
 
 mystack = Stack()
 mystack.append_stack(1)
 mystack.append_stack(2)
 mystack.append_stack(3)
 mystack.append_stack(4)
-# mystack.enqueue(5)
+mystack.enqueue(5)
+# mystack.dequeue()
 mystack.print_stack()

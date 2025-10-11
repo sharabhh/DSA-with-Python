f27c4c130a6fe9fdef9a80f35fdbb648bdb67f21
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-11 14:00:06 +0530

    add first_non_repeating_char function to find the first non-repeating character in a string

---
+++ Hash Maps/4_first_non_repeat.py
@@ -0,0 +1,16 @@
+def first_non_repeating_char(str):
+    my_dict = {}
+
+    for i in str:
+        if i not in my_dict:
+            my_dict[i] = 1
+        else:
+            my_dict[i] +=1
+    
+    for key, value in my_dict.items():
+        if value == 1:
+            return key
+    
+    return None
+
+print(first_non_repeating_char("hheello"))

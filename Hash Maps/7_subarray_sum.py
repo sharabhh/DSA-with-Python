2bb2f6afa31cf9533d904f8370a9c005168703fb
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-13 00:15:32 +0530

    added question

--- Hash Maps/7_subarray_sum.py
+++ Hash Maps/7_subarray_sum.py
@@ -1,3 +1,9 @@
+# Given an array of integers nums and a target integer target, write a Python function called subarray_sum that finds the indices of a contiguous subarray in nums that add up to the target sum using a hash table (dictionary).
+# Your function should take two arguments:
+# nums: a list of integers representing the input array
+# target: an integer representing the target sum
+# Your function should return a list of two integers representing the starting and ending indices of the subarray that adds up to the target sum. If there is no such subarray, your function should return an empty list.
+
 def subarray_sum(nums, target):
     arr_map = { 0 : -1}
     sum = 0

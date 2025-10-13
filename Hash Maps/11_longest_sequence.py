e67a9e5111d28add7926f41f69fe3b8d2d65ba20
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-13 20:12:01 +0530

    refactor longest_consecutive_sequence function to use num_set for improved performance

--- Hash Maps/11_longest_sequence.py
+++ Hash Maps/11_longest_sequence.py
@@ -7,10 +7,10 @@ def longest_consecutive_sequence(nums):
     num_set = set(nums)
     longest_sequence = 0
 
-    for num in nums:
-        if num - 1 not in nums:
-            current_num = 0
-            current_sequence = 0
+    for num in num_set:
+        if num - 1 not in num_set:
+            current_num = num
+            current_sequence = 1
 
             while current_num +1 in num_set:
                 current_num +=1

fb2e25f913e592c4d353b480b8fc4d391cdb8a3c
sharabh mishra <sharabhmishra15@gmail.com>
2025-10-13 20:06:07 +0530

    added question

--- Hash Maps/10_find_pairs.py
+++ Hash Maps/10_find_pairs.py
@@ -1,3 +1,7 @@
+# You are given two lists of integers, arr1 and arr2, and a target integer value, target. Your task is to find all pairs of numbers (one from arr1 and one from arr2) whose sum equals target.
+# Write a function called find_pairs that takes in three arguments: arr1, arr2, and target, and returns a list of all such pairs.  Assume that each array does not contain duplicate values.
+# The tests for this exercise assume that arr1 is the list being converted to a set.
+
 def find_pairs(arr1, arr2, target):
     set1 = set(arr1) # this will avoid repeatation
     final_list = []
